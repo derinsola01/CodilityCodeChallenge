0afdd93292d9f564bd7ada639982201201d7110d
Derin Gbadebo <daireenonline@yahoo.com>
2019-09-26 12:06:24 -0700

    Refactored to remove Java warning messages.

--- src/test/java/com/derinplayground/codilitycodingchallenge/september2019/PassingCarsTest.java
+++ src/test/java/com/derinplayground/codilitycodingchallenge/september2019/PassingCarsTest.java
@@ -5,13 +5,11 @@ import com.derinplayground.codilitycodingchallenge.september2019.PassingCars;
 
 public class PassingCarsTest extends TestCase {
 	
-	private static PassingCars passingCars;
-	
     public void testPassingCarsPairs(){
     	int[] array1 = {0, 1, 0, 1, 1, 0, 1, 1};
     	int[] array2 = {0, 1, 0, 1, 1};
-        assertTrue(passingCars.passingCarsPairs(array1) == 11);
-        assertTrue(passingCars.passingCarsPairs(array2) == 5);
+        assertTrue(PassingCars.passingCarsPairs(array1) == 11);
+        assertTrue(PassingCars.passingCarsPairs(array2) == 5);
     }
 
 }
