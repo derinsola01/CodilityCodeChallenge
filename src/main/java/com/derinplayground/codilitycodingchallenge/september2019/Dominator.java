8def7d82805b2b8cd08da6cb59965e6e44fa752d
Derin Gbadebo <daireenonline@yahoo.com>
2020-04-04 17:08:30 -0700

    upgraded to log4j2

--- src/main/java/com/derinplayground/codilitycodingchallenge/september2019/Dominator.java
+++ src/main/java/com/derinplayground/codilitycodingchallenge/september2019/Dominator.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Dominator {
 	
--- src/main/java/com/derinplayground/codilitycodingchallenge/september2019/MissingInteger.java
+++ src/main/java/com/derinplayground/codilitycodingchallenge/september2019/MissingInteger.java
@@ -3,8 +3,8 @@ package com.derinplayground.codilitycodingchallenge.september2019;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MissingInteger {
 	
